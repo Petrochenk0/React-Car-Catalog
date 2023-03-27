a9c70d35e167f1e436636b20a16a4731cdc33edf
Petrochenk0 <96386665+Petrochenk0@users.noreply.github.com>
2023-03-27 10:30:18 +0300

    Update CreateForm.jsx
    add function create in console

--- car-catalog/src/components/screens/home/create-car-form/CreateForm.jsx
+++ car-catalog/src/components/screens/home/create-car-form/CreateForm.jsx
@@ -1,8 +1,28 @@
 import styles from './CreateForm.module.css'
+import { useState } from 'react'
+
 const CreateForm = () =>{
-     return <form className={styles.form}>
-        <input type="text" placeholder="name" />
-    </form>
+
+    const [name, setName] = useState('')
+    const createCar = e => {
+        e.preventDefault()
+        console.log(name)
+    }
+
+    
+     return(
+        <form className={styles.form}>
+            <input 
+            placeholder="name" 
+            onChange={e => setName(e.target.value)}
+            value={name}/>
+            <input placeholder="price" />
+            <input placeholder="img" />
+
+            <button onClick={e=>createCar(e)}>Create</button>
+        </form>
+     ) 
+
 }
 
 export default CreateForm
